7d0b21d5101e23497de847bcf5bc8f47da41fe6e
Mads Bo Mikkelsen <au615041@uni.au.dk>
2020-04-01 08:43:22 +0200

    Add project files.

---
+++ OnlineHelpSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OnlineHelpSystem
+{
+    // test
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
