f08dbb0d229dd6447163f81d5b592aa3671faa85
Quynh Trang Mac <201810269@post.au.dk>
2020-04-01 10:04:41 +0200

    Changes in myDBcontex making the relations for Teachers and implementated the teacher-class

--- OnlineHelpSystem/Models/Teacher.cs
+++ OnlineHelpSystem/Models/Teacher.cs
@@ -6,11 +6,27 @@ namespace OnlineHelpSystem.Models
 {
     public class Teacher
     {
-        public string Name { get;  set; }
+        public string name { get;  set; }
         public string AuId { get;  set; }
 
         public List<Exercise> Exercises { get;  set; }
+        public List<Assignment> Assignments { get; set; }
 
         /*Make prints out*/
+        public override string ToString()
+        {
+            var Assigments = "";
+            if (Assigments!=null)
+            {
+                Assigments = string.Join(";", Assigments);
+            }
+            var Exercises = "";
+            if (Exercises!=null)
+            {
+                Exercises = string.Join(";", Exercises);
+            }
+
+            return string.Format("Teacher ({0},{1},{2},{3})", name, AuId, Exercises, Assigments);
+        }
     }
 }
