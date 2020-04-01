59d27c74f36103ff87aefa1b121fb4cb02eb7fa0
Mads Bo Mikkelsen <au615041@uni.au.dk>
2020-04-01 09:56:48 +0200

    added properties to shadowtabel StudentCourse

--- OnlineHelpSystem/Models/StudentCourse.cs
+++ OnlineHelpSystem/Models/StudentCourse.cs
@@ -6,6 +6,18 @@ namespace OnlineHelpSystem.Models
 {
     public class StudentCourse
     {
+        public bool Active { get; set; }
+
+        public int Semester { get; set; }
+
+        public Student student { get; set; }
+        public string studentAuId { get; set; }
+
+
+        public Course course { get; set; }
+
+        public int courseId { get; set; }
+
 
     }
 }
