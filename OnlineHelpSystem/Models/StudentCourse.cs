6738368485a19dc771ad7f21c6777b0041d3f12e
Mads Bo Mikkelsen <au615041@uni.au.dk>
2020-04-01 10:02:30 +0200

    updated StudenCourse

--- OnlineHelpSystem/Models/StudentCourse.cs
+++ OnlineHelpSystem/Models/StudentCourse.cs
@@ -6,17 +6,18 @@ namespace OnlineHelpSystem.Models
 {
     public class StudentCourse
     {
+        public int StudentCourseId { get; set; }
         public bool Active { get; set; }
 
         public int Semester { get; set; }
 
-        public Student student { get; set; }
-        public string studentAuId { get; set; }
+        public Student Student { get; set; }
+        public string StudentAuId { get; set; }
 
 
-        public Course course { get; set; }
+        public Course Course { get; set; }
 
-        public int courseId { get; set; }
+        public int CourseId { get; set; }
 
 
     }
